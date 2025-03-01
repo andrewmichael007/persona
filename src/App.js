b9f7509506f519a40a93a118a92b0d3cd9424a04
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-03-01 07:31:11 +0000

    rendering home.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Test from "./Components/Test";
+import MyNavbar from "./Components/Home"
 
 const App = () => {
     return(
         <div>
             <Test />
+        </div>
+        <div>
+            <MyNavbar />
         </div>
     );
 };
