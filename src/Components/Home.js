523c4095a3279c57e2176cfc8ab2b53f98d36d3a
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-03-01 07:28:24 +0000

    creation of home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,1 +1,14 @@
+import React from "react";
+import "../Styles/App.css";
 
+const MyNavbar = () => {
+    return(
+        <ol
+            <li>Banana</li>
+            <li>Guava</li>
+            <li>Pear</li>
+        </ol>
+    );
+};
+
+export default MyNavbar;
