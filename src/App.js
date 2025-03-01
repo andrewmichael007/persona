c5ca1d788c06cce45a70fc057bd9752ac90bbf1a
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-03-01 06:41:32 +0000

    deleted all codes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,1 @@
-// import logo from './logo.svg';
-// import './App.css'
-import React from "react";
-import Test from "./Components/Test";
 
-function App() {
-  return (
-    <div>
-        <Test />
-    </div>
-    
-  );
-}
-
-export default App;
