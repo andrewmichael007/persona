1eefd73048da4271aa49ff5f29fc58493d5f49ad
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-03-01 07:58:06 +0000

    commented out rendering of test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Test from "./Components/Test";
+// import Test from "./Components/Test";
 import MyNavbar from "./Components/Home";
 import "./Styles/App.css";
 
@@ -6,7 +6,7 @@ import "./Styles/App.css";
 const App = () => {
     return(
         <div>
-            <Test />
+            // <Test />
 
             <MyNavbar />
         </div>
