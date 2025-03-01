0da37957e65f4f54e06d153f31dcd0f661d557ca
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-03-01 09:32:01 +0000

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,34 +4,41 @@ import "../Styles/App.css";
 const MyNavbar = () => {
     return(
         <header>
-                <div className = "logo-container">
-                    <img src = " " alt = "logo" />
-                    <h4> Andy </h4>
-                </div>
                 <nav className = "navbar-container">
-                    
-                    <div className = "nav opt1">
-                        <a href = " " target = "_self">  
-                             <h4> Home </h4>
-                        </a>
+        
+                    <div className = "logo-container">
+                        <img src = " " alt = "logo" />
+                        <h4> Andy </h4>
                     </div>
-                    <div className = "nav opt2">
-                        <a href = " " target = "_self">
-                            <h4> Coming Soon </h4>
-                        </a>
-                    </div>
-                    <div className = "nav opt3">
-                        <a href = " " target = "_self">
-                            <h4> Blog </h4>
-                        </a>
-                    </div>
-                    <div className = "nav opt4">
-                        <a href = " " target = "_self">
-                            <h4> Contact Me </h4>
-                        </a>
-                    </div>
-                  </nav>
+        
+                    <div className = "options-container">
+        
+                        <div className = "nav opt1">
+                            <a href = " " target = "_self">  
+                                <h4> Home </h4>
+                            </a>
+                        </div>
+                        <div className = "nav opt2">
+                            <a href = " " target = "_self">
+                                <h4> Coming Soon </h4>
+                            </a>
+                        </div>
+                        <div className = "nav opt3">
+                            <a href = " " target = "_self">
+                                <h4> Blog </h4>
+                            </a>
+                        </div>
+                        <div className = "nav opt4">
+                            <a href = " " target = "_self">
+                                <h4> Contact Me </h4>
+                            </a>
+                        </div>
+        
+                    </div> 
+        
+                 </nav>
         </header>
+                
     );
 };
 
