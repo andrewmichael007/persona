634d3192461af1136dcb94a0a1c62cf295c6c323
Andy-Techy <adjeimitchel03@gmail.com>
2025-03-29 15:51:45 +0000

    finish changing the whole code base of my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
 // import Test from "./Components/Test";
-import React, { useState } from "react";
-import { MyNavbar, Main, Footer, Sidebar } from "./Components/Home";
+import React from "react";
 import "./Styles/App.css";
 
 
+import MyNavbar from "./Components/Home/Navbar/Navbar.jsx";
+import "./Components/Home/Navbar/Navbar.module.css";
+
+import Main from "./Components/Home/Main/Main.jsx";
+import  "./Components/Home/Main/Main.module.css";
+
+
+import Footer from "./Components/Home/Footer/Footer.jsx";
+import  "./Components/Home/Footer/Footer.module.css";
+
+
+// import Sidebar from "./Components/Home/Sidebar/Sidebar.jsx";
+// import  "./Components/Home/Sidebar/Sidebar.module.css";
+
+
+
+
 const App = () => {
-    // State to manage sidebar visibility
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    // // State to manage sidebar visibility
+    // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
 
-    // Function to toggle the sidebar
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen); 
-    };
+    // // Function to toggle the sidebar
+    // const toggleSidebar = () => {
+    //     setIsSidebarOpen(!isSidebarOpen); 
+    // };
     
-       // Function to close the sidebar
-    const closeSidebar = () => {
-        setIsSidebarOpen(false);
-    };
+    //    // Function to close the sidebar
+    // const closeSidebar = () => {
+    //     setIsSidebarOpen(false);
+    // };
     
 
     return(
         <div>
             <div>
                 {/* Pass toggleSidebar function to MyNavbar */}
-                <MyNavbar toggleSidebar={toggleSidebar} />
+                <MyNavbar />
             </div>
             
 
             <div>
                 {/* Sidebar component with toggle functionality */}
-                <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+                {/* <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} /> */}
 
                 <Main />
             </div>
---
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import logo from "../../../Assets/logo.svg";
+
+const MyNavbar = () => {
+    return(
+        <header className={styles.header}>
+            <nav className={styles.navbar}>
+                {/* Bars container */}
+                <div className={styles.barsContainer}>
+                    <div className={styles.topBar}></div>
+                    <div className={styles.midBar}></div>
+                    <div className={styles.bottomBar}></div>
+                </div> 
+
+                {/* Logo section */}
+                <div className={styles.logoContainer}>
+                    <img src={logo} alt="logo" className={styles.logo} />
+                    {/* <h4 className={styles.logoText}>NDY</h4> */}
+                    <div className={styles.dot}></div>
+                </div>
+
+                {/* Divider */}
+                <div className={styles.divider}></div>
+            
+                {/* Navigation options */}
+                <div className={styles.optionsContainer}>
+                    <div className={styles.navItem}>
+                        <a href="/" target="_self" className={styles.navLink}>  
+                            <h4> Home </h4>
+                        </a>
+                    </div>
+                    <div className={styles.navItem}>
+                        <a href="/soon" target="_self" className={styles.navLink}>
+                            <h4> Learning </h4>
+                        </a>
+                    </div>
+                    <div className={styles.navItem}>
+                        <a href="/blog" target="_self" className={styles.navLink}>
+                            <h4> About </h4>
+                        </a>
+                    </div>
+                    <div className={styles.navItem}>
+                        <a href="/about" target="_self" className={styles.navLink}>
+                            <h4> Contact </h4>
+                        </a>
+                    </div>
+                </div> 
+
+                {/* Theme toggle */}
+                <div className={styles.toggleContainer}>
+                    <div className={styles.toggleParent}></div>
+                    <div className={styles.toggleChild}></div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default MyNavbar;
