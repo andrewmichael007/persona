c50782d475e2a5306168d06ec37a96fc995665f0
Andy-Techy <adjeimitchel03@gmail.com>
2025-04-15 23:55:27 +0000

    added a learning file

---
+++ src/Components/Learning/Learning.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import styles from  "./Learning.module.css";
+
+
+const Test  = () => {
+    return(
+        <div >
+            <h1> Testing my Learning page </h1>
+        </div>
+    );
+};
+
+export default Test;
