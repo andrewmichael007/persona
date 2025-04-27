b1c679abec2342aeeda41b6bc6963d91c65f0676
Michael Andrew <adjeimitchel03@gmail.com>
2025-04-27 11:34:05 +0000

    commented all footer codes

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import styles from "./Footer.module.css";
+// import React from "react";
+// import styles from "./Footer.module.css";
 
-const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.footerContent}>
-                <div className={styles.brandMark}>Andy</div>
-                <div className={styles.copyright}>
-                    &copy; {new Date().getFullYear()}
-                </div>
-            </div>
-        </footer>
-    );
-};
+// const Footer = () => {
+//     return (
+//         <footer className={styles.footer}>
+//             <div className={styles.footerContent}>
+//                 <div className={styles.brandMark}>Andy</div>
+//                 <div className={styles.copyright}>
+//                     &copy; {new Date().getFullYear()}
+//                 </div>
+//             </div>
+//         </footer>
+//     );
+// };
 
-export default Footer;
+// export default Footer;
