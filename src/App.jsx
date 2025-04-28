523461527695ff3f4c92053782022b21e8e3bf7a
Michael Andrew <adjeimitchel03@gmail.com>
2025-04-28 00:02:45 +0000

    rendering stuff

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,32 @@ import "./Styles/App.css";
 
 import MyNavbar from "./Components/Home/Navbar/Navbar.jsx";
 import "./Components/Home/Navbar/Navbar.module.css";
+
+
+
+
+const App = () => {
+    return(
+        <Router>
+            <div>
+                {/* Pass toggleSidebar function to MyNavbar */}
+                <MyNavbar />
+            </div>
+
+            <Routes>
+                <Route  path = "/learning" element = { <Test /> } />
+            </Routes>
+            
+            <div>
+                {/* Sidebar component with toggle functionality */}
+                {/* <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} /> */}
+
+                <Main />
+            </div>
+        
+                <Footer /> 
+        </Router>
+    );
+};
+
+export default App;
