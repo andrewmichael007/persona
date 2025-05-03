4874146139bc5c8841562cf668e8182a3c8060e6
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-03 20:22:21 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+// src/index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './vitals';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals(sendToVercelAnalytics);
+
+
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
@@ -16,9 +36,9 @@
 
 
 // src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
