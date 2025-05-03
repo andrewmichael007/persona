dd2514657a477f7fdec2261d089fdd44bee8d02e
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-03 19:26:12 +0000

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,1 +1,23 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <div className="navbar__logo">
+        <Link to="/">DevPortfolio</Link>
+      </div>
+      <nav className="navbar__links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/learning">Learning</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
 
