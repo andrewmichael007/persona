596516a5f81f59ac5e6ed44fa3cd77a1cfa0d844
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-05 19:00:07 +0000

    code review, commenting out

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,23 @@
-// src/components/Navbar.jsx
+// src/Components/Navbar.jsx
 import React from 'react';
+//import link from react-router-dom because our navbar items are goin' to be links
 import { Link } from 'react-router-dom';
 import '../Styles/Navbar.css';
 
 const Navbar = () => {
   return (
+    //return a semantic tag of header
     <header className="navbar">
-      <div className="navbar__logo">
-        <Link to="/"> mitchPersona </Link>
+      {/* logo section */}
+      <div className="logo">
+        <Link to="/"> portfolio1.0 </Link>
       </div>
-      <nav className="navbar__links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/learning">Learning</Link>
-        <Link to="/contact">Contact</Link>
+      {/* a list of nav items */}
+      <nav className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/learning"> Learning </Link>
+        <Link to="/contact"> Contact </Link>
       </nav>
     </header>
   );
