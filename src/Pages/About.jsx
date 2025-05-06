1700b71e97582707d875bcd51665982fd877f05a
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-06 14:19:54 +0000

    code maintenance: merged framework to techstack

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,53 +7,53 @@ import profile from '../Assets/profilePic.png';
 const About = () => {
   //creating a dictionary of techstack. Could be updated anytime
   const techStack = [
-     "HTML5", "CSS3", "JavaScript", "PHP", "Python", "MySQL", "MongoDB" 
+     "HTML5", "CSS3", "JavaScript", "PHP", "Python", "MySQL", "MongoDB",
+
+     "React", "Node.js", "Express.js", "Next.js", "Numpy", "Matplotlib", "Pandas", 
+
+     "Vercel", "Git"
+
   ];
 
   //creaing a dictionary of frameworks
-  const frameworks = [
-    "React", "Node.js", "Express.js", "Next.js"
- ];
+//   const frameworks = [
+//     "React", "Node.js", "Express.js", "Next.js"
+//  ];
 
   return (
     // the whole about container 
     <section className="about-container">
-{/*       //dividing the container into two  */}
       <div className="about-grid">
+        <div className="about">
+          <div className = "persona">
+            <h2 className="about-heading"> About Me </h2>
+            <p>
+              I respond to Mitch😎
+            </p>
+            <p> 
+                I’m a learner currently exploring best global practices and emerging trends in the tech industry.
+                Data modelling, system design and building scalable web applications.🚀
+            </p>
 
-{/*         the left side of the container containing the about heading, paragraph, tech stack, frameworks and all */}
-        <div className="about"> 
-          <h2 className="about-heading"> About Me </h2>
-          <p>
-            I respond to Mitch😎
-          </p>
-          <p> 
-              I’m a learner currently exploring best global practices and emerging trends in the tech industry.
-              System Design and building scalable web applications.🚀
-          </p>
-
-          <p>
-            Machine Learning, with a focus on Computer Vision
-            Healthcare Technology, where I aim to apply ML and CV for real-world impact
-            I’m continuously learning, working and growing toward becoming a well-rounded computer engineer who builds solutions that matter.✨
-          </p>
-
-          <h3 className="skills-heading"> Tech Stack </h3> 
-          <div className="skills-list">
-            {techStack.map((stack, index) => (
-              <span className="skill-framework-tag" key={index}>{stack}</span>
-            ))}
+            <p>
+              Machine Learning, with a focus on Computer Vision
+              Healthcare Technology, where I aim to apply ML and CV for real-world impact
+              I’m continuously learning, working and growing toward becoming a well-rounded computer engineer who builds solutions that matter.✨
+            </p>
           </div>
-
-          <h3 className = "frameworks-heading"> Frameworks </h3>
-          <div className = "frameworks-list">
-            {frameworks.map((framework, index) => (
-              <span className = "skill-framework-tag" key={index}>{framework}</span>
-            ))}
+                   
+          {/* <h3 className = "stack-heading"> Tech Stack </h3> */}
+          <div className = "techstack-container">
+            <h3 className = "stack-heading"> Tech Stack </h3>
+            <div className="techstack-list">
+              {techStack.map((stack, index) => (
+                <span className="techstack-tag" key={index}>{stack}</span>
+              ))}
+            </div>
           </div>
         </div>
 
-        <div className="about-visuals">
+        <div className="visuals">
           <img src={profile} alt="Mitch Profile" className="profile-img" />
           <div className="fun-facts">
             <p> 🤖 tech enthusiast   + 🎵 music head </p>
