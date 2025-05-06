d799c2ca0f8f4b3254c654f3574a6ed6f5e831f5
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-06 07:05:23 +0000

    code maintenance : adding emojis to paragraph extracts, commenting out

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,35 +5,40 @@ import profile from '../Assets/profilePic.png';
 
 // creating an arrow function to hold the about page
 const About = () => {
-  
+  //creating a dictionary of techstack. Could be updated anytime
   const techStack = [
      "HTML5", "CSS3", "JavaScript", "PHP", "Python", "MySQL", "MongoDB" 
   ];
 
+  //creaing a dictionary of frameworks
   const frameworks = [
     "React", "Node.js", "Express.js", "Next.js"
  ];
 
   return (
+    // the whole about container 
     <section className="about-container">
+{/*       //dividing the container into two  */}
       <div className="about-grid">
-        <div className="about-text">
+
+{/*         the left side of the container containing the about heading, paragraph, tech stack, frameworks and all */}
+        <div className="about"> 
           <h2 className="about-heading"> About Me </h2>
           <p>
-            I respond to Mitch. 
+            I respond to Mitch😎
           </p>
           <p> 
-              I’m a passionate learner currently exploring best practices and emerging trends in the tech industry. My interests lie in:
-              System Design and building scalable web applications. 
+              I’m a learner currently exploring best global practices and emerging trends in the tech industry.
+              System Design and building scalable web applications.🚀
           </p>
 
           <p>
             Machine Learning, with a focus on Computer Vision
             Healthcare Technology, where I aim to apply ML and CV for real-world impact
-            I’m continuously growing, experimenting, and working toward becoming a well-rounded computer engineer who builds solutions that matter.
+            I’m continuously learning, working and growing toward becoming a well-rounded computer engineer who builds solutions that matter.✨
           </p>
 
-          <h3 className="skills-heading"> Tech Stack </h3>
+          <h3 className="skills-heading"> Tech Stack </h3> 
           <div className="skills-list">
             {techStack.map((stack, index) => (
               <span className="skill-framework-tag" key={index}>{stack}</span>
@@ -51,7 +56,7 @@ const About = () => {
         <div className="about-visuals">
           <img src={profile} alt="Mitch Profile" className="profile-img" />
           <div className="fun-facts">
-            <p>tech enthusiast   + 🎵 music head </p>
+            <p> 🤖 tech enthusiast   + 🎵 music head </p>
           </div>
         </div>
       </div>
