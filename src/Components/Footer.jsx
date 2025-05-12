a315faa671b3eb414af95bb23160853532fcd03f
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-12 23:52:42 +0000

    adding icons to footer socials

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,10 @@
 // src/components/Footer.jsx
 import React from 'react';
 import '../Styles/Footer.css';
+import github from "../Assets/github.png";
+import linkedin from "../Assets/linkedin.png";
+import gmail from "../Assets/gmail.png";
+import instagram from "../Assets/instagram.png";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -9,10 +13,27 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="content">
         <div className="links">
-          <a href="https://github.com/Yo-mitch" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="micheleandreas00@gmail.com">Email</a>
-          <a href = "https://www.instagram.com/_mitchcoco/?hl=en" target = "_blank" rel = "noopener noreferrer"> Instagram </a>
+          <div>
+            <a href="https://github.com/Yo-mitch" target="_blank" rel="noopener noreferrer"> Github
+            <img src={github} alt = "github" />
+            </a>
+          </div>
+          <div className = "linkedin-holder">
+            <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn
+            <img src={linkedin} alt = "linkedin" />
+            </a>
+          </div>
+          <div className = "gmail-holder">
+            <a href="micheleandreas00@gmail.com">Email
+            <img src={gmail} alt = "gmail-holder" />
+            </a>
+          </div>
+          <div className = "github-holder">
+            <a href = "https://www.instagram.com/_mitchcoco/?hl=en" target = "_blank" rel = "noopener noreferrer"> Instagram 
+            <img src={instagram} alt = "instagram" />
+            </a>
+          </div>
+
         </div>
         <p>&copy; {currentYear} mitch. All rights reserved.</p>
       </div>
