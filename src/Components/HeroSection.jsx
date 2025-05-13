ef771441ca50bd6e764d632e93c36787e63bb307
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-13 06:45:18 +0000

    adding animations to herosection

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -17,16 +17,6 @@ const HeroSection = () => {
         </p>
       </div>
       
-
-      {/* <div className="hero-buttons">
-        <a href="/resume.pdf" className="btn primary-btn" target="_blank" rel="noopener noreferrer">
-          View Resume
-        </a>
-        <a href="#projects" className="btn secondary-btn">
-          My Projects
-        </a>
-      </div> */}
-
       <div className="scroll-down">
         <span>&#8595;</span>
       </div>
