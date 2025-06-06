6afdd11e11d4c428fccaafd13b089f7ababa0d36
Andy-Techy <adjeimitchel03@gmail.com>
2025-06-06 16:38:28 +0000

    creating a full portfolio web app

---
+++ backend/routes/contact.js
@@ -0,0 +1,73 @@
+// this is  the contact route function
+// /routes/contact.js
+
+const express = require("express");
+const router = express.Router();
+const schema = require("../models/schema")
+const { body, validationResult } = require("express-validator");
+const rateLimit = require("express-rate-limit");
+const axios = require("axios");
+
+
+// defining rate limiter to limit the number of requests
+const rateLimiter = rateLimit({
+    //window time
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    //maximum number of requests
+    max : 5,
+    message: {
+        success: false,
+        msg: "too many requests, try again after an hour"
+    }
+});
+
+// routing the contact.js component
+router.post(
+    "/",
+    //calling rateLimiter
+    rateLimiter,
+
+    //validator
+    [
+        body('name').trim().notEmpty().isLength({ min: 5 }).withMessage("name is required"),
+        body('email').isEmail().withMessage("invalid email"),
+        body('message').trim().notEmpty().withMessage("message is required"),
+        body('recaptchaToken').notEmpty().withMessage('reCAPTCHA is required')
+    ],
+
+    async(req,res) => {
+        
+        //checking for errors
+        const errors = validationResult(req);
+        if(!errors.isEmpty() ){
+            return res.status(400).json({ success : false , errors: errors.array() });
+        }
+
+        const { name , email, message, recaptchaToken } = req.body;
+        
+        // Verify reCAPTCHA
+        const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${recaptchaToken}`;
+
+        try {
+            const captchaResponds = await axios.post(verifyURL);
+            
+            if (!captchaResponds.data.success) {
+                return res.status(400).json({ success: false, msg: 'Failed reCAPTCHA verification' });
+            }
+
+            //saving messages to mongo
+            const newSchema = new schema({name, email, message});
+            await newSchema.save();
+
+            res.status(200).json({success : true, msg: "message sent"});
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ success: false, msg: 'Server error' });
+        }
+});
+
+// export the contact route function
+module.exports = router;
+
+
