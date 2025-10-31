0cc31aa8b5b3cbfdbeb66384d8d92c720220dbc7
Andy-Techy <adjeimitchel03@gmail.com>
2025-10-31 08:59:33 +0000

    did a code review on the main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 // src/App.jsx
+// import React from 'react';
 
-import React from 'react';
 //importing Router, Routes and Route to hold links of the navbar from react-router-dom
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 //importing various components
 import Navbar from './Components/Navbar';
 import HeroSection from './Components/HeroSection';
 import Footer from './Components/Footer';
+
 //importing various pages
 import About from './Pages/About';
 import Learning from './Pages/Learning';
 import Contact from './Pages/Contact';
+
 //importing the global styles
 import './Styles/App.css';
 
