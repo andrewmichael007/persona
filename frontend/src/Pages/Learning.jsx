06524cb621cdc295edfcb7fb924d0b0283e56d51
Andy-Techy <adjeimitchel03@gmail.com>
2025-11-01 23:56:13 +0000

    code review

--- frontend/src/Pages/Learning.jsx
+++ frontend/src/Pages/Learning.jsx
@@ -8,60 +8,70 @@ const learningItems = [
     status: 'In Progress',
     link: "https://www.coursera.org/learn/machine-learning"
   },
+
   {
     title: "Health-labs1.0 : AI-Powered Health Chatbot",
     platform: "Personal Project",
     status: 'Building',
     link: "https://www.coursera.org/learn/machine-learning"
   },
+
   {
     title: "Machine Learning",
     platform: "Coursera",
     status: 'In Progress',
     link: "https://www.coursera.org/learn/machine-learning"
   },
+
   {
     title: "Full Stack MERN Development",
     platform: "Book",
     status: "Learning",
     link: '#'
   },
+  
   {
     title: "CRUD App",
     platform: "Youtube",
     status: "Building",
     link: '#'
   },
+
   {
     title: "Designing Data Intensive Applications by Martin Kleppmann",
     platform: "Book",
     status: "Reading",
     link: '#'
   },
+
   {
     title: 'Clean Code by Robert C. Martin',
     platform: 'Book',
     status: 'Reading',
     link: '#'
   },
+
   {
     title: "Head First Design Patterns: Building Extensible and Maintainable Softwares",
     platform: "Book",
     status: "Reading",
     link: '#'
   },
+
   {
     title: "Computational Science Distilled by  Wladston Ferreira Filho",
     platform: "Book",
     status: "Reading",
     link: '#'
   },
+
   {
     title: "Mastering System Design",
     platform: "Online Article",
     status: "Learning",
     link: "https://blog.algomaster.io/p/30-system-design-concepts"
-  },
+  }
+  
 ];
 
 const Learning = () => {
